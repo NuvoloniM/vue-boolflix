065575c9f98600deb46ddcf4aa15d8a9f79d7522
NuvoloniM <nuvoloni.m@gmail.com>
2022-04-22 14:57:43 +0200

    Start

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import "bootstrap";
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+@import "bootstrap/dist/css/bootstrap.min.css";
+</style>
