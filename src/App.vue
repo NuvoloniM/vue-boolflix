caf1ebab09fb24af0862594aee36a7685c84f92e
NuvoloniM <nuvoloni.m@gmail.com>
2022-04-25 21:03:22 +0200

    telefilm axios and telefilm card comp

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,22 @@
     </header>
     <main>
       <div class="row">
+        <h2 v-if="this.filteredFilm.length > 0" class="ps-3 py-2">Film</h2>
         <FilmCardComp
+        :personalKey="personalKey"
         v-for="element in filteredFilm"
         :key="element.id"
         :title=element.original_title
+        :overview="element.overview"
+        :image="element.poster_path"
+        />
+      </div>
+      <div class="row">
+        <h2 v-if="this.filteredTelefilm.length > 0" class="ps-3 py-2">Telefilm</h2>
+        <TelefilmCardComp
+        v-for="element in filteredTelefilm"
+        :key="element.id"
+        :name="element.name"
         :overview="element.overview"
         :image="element.poster_path"
         />
@@ -27,19 +39,24 @@ import axios from 'axios';
 import LogoComp from "./components/header/LogoComp.vue";
 import InputComp from "./components/header/InputComp.vue";
 import FilmCardComp from "./components/main/FilmCardComp.vue";
+import TelefilmCardComp from "./components/main/TelefilmCardComp.vue";
 
 export default {
   name: 'App',
   data() {
     return{
+      personalKey: 'ac7ccea2c466d848c3c9dc330aad7c80',
       inputFilm: '',
       filteredFilm: [],
+      inputTelefilm: '',
+      filteredTelefilm:[],
     }
   },
   components: {
     LogoComp,
     InputComp,
     FilmCardComp,
+    TelefilmCardComp,
   },
   created() {
   },
@@ -47,10 +64,17 @@ export default {
     inputResultFunction(testo){
       console.log(testo);
       this.inputFilm = testo;
-      axios.get( `https://api.themoviedb.org/3/search/movie?api_key=ac7ccea2c466d848c3c9dc330aad7c80&language=it-IT&query=${this.inputFilm}` )
+      axios.get( `https://api.themoviedb.org/3/search/movie?api_key=${this.personalKey}&language=it_IT&query=${this.inputFilm}` )
         .then( (res) =>{
           console.log(res.data.results);
           this.filteredFilm = res.data.results;
+        }
+      )
+      this.inputTelefilm = testo;
+      axios.get( `https://api.themoviedb.org/3/search/tv?api_key=${this.personalKey}&language=it_IT&query=${this.inputTelefilm}` )
+        .then( (res) =>{
+          console.log(res.data.results);
+          this.filteredTelefilm = res.data.results;
         }
       )
     }
---
+++ src/components/main/TelefilmCardComp.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card col-2 my-2 p-0">
+        <img :src="`${imgSize}${image}`" class="img-fluid" alt="...">
+        <!-- <div class="card-body">
+            <h5 class="card-title"> {{title}} </h5>
+            <p class="card-text"> {{overview}} </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div> -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TelefilmCardComp',
+    data(){
+        return {
+            imgSize: 'https://image.tmdb.org/t/p/w500',
+        }
+    },
+    props: {
+        name: String,
+        overview: String,
+        image: String,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .card{
+        border-color: rgb(200, 187, 187);
+    }
+</style>
